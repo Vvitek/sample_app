c4601b23a7ddea30ee8f5d2bc526d15a05303c6f
Vvitek <wifacz@gmail.com>
2017-07-21 09:26:28 +0200

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
+  end
+
+  test "valid signup informations" do 
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "test",
+                                         email: "user@valid.com",
+                                         password:              "foo123",
+                                         password_confirmation: "foo123" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
