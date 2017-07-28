e6e9dabcf0bb0c7d72de0de685f8682522074ffe
Vvitek <wifacz@gmail.com>
2017-07-28 08:31:13 +0200

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,13 @@ class User < ApplicationRecord
 	  BCrypt::Password.create(string, cost: cost)
   end
 
+  def authenticated?(remember_token)
+	  return false if remember_digest.nil?
+	  BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+	  update_attribute(:remember_digest, nil)
+  end
+
 end
