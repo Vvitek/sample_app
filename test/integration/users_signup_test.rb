7507544471c6ce4696380f7171137d7e967072eb
Vvitek <wifacz@gmail.com>
2017-08-07 09:38:02 +0200

    Add acount activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+	def setup
+		ActionMailer::Base.deliveries.clear
+	end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -16,17 +20,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert-danger'
   end
 
-  test "valid signup informations" do 
+  test "valid signup informationsi with account activation" do 
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "test",
-                                         email: "user@valid.com",
-                                         password:              "foo123",
-                                         password_confirmation: "foo123" } }
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
-    assert_template 'users/show'
-    assert_not flash.empty?
     assert is_logged_in?
   end
 end
